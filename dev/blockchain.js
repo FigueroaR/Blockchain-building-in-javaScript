f1ead548f406f182d04a713476f364dd3ca73a98
figueroar <raul.a.figueroa@hotmail.com>
2020-06-17 19:31:11 -0400

    .createNewBlock test passing

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,7 +3,7 @@ function Blockchain() {
   this.newTransactions = [];
 }
 
-Blockchain.prototype.crateNewBlock = function(nonce, previousBlockHash, hash) {
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
   const newBlock = {
     index: this.chain.length + 1,
     timestamp: Date.now(),
--- dev/test.js
+++ dev/test.js
@@ -1,5 +1,7 @@
 const Blockchain = require("./blockchain");
 
-const blockchain = require('./blockchain');
-const bitcoin = new Blockchain();
+const BlockChain = require('./blockchain');
+const bitcoin = new BlockChain();
+bitcoin.createNewBlock(10257,"GHGURFUFU", 'FSDFSDF45216')
+bitcoin.createNewBlock(45120,"GFD4SFSD4", 'RGRG565GR6D6')
 console.log(bitcoin)
