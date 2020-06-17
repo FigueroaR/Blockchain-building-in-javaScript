45c2b7de9eeb08134047b3ac082b8559323042a8
figueroar <raul.a.figueroa@hotmail.com>
2020-06-17 18:58:03 -0400

    createNewBlock method added to Blockchain

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -2,3 +2,15 @@ function Blockchain () {
   this.chain = [];
   this.newTransactions = [];
 }
+
+Blockchain.prototype.crateNewBlock = function(nonce, previousBlockHash, hash) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash
+  }
+  return newBlock;
+}
