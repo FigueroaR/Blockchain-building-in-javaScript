ceeb6d59a4fc55c02b5e474ead63e9961f8bfcb3
figueroar <raul.a.figueroa@hotmail.com>
2020-06-30 17:44:27 -0400

    newTrnsaction method tested

--- dev/test.js
+++ dev/test.js
@@ -1,7 +1,15 @@
-const Blockchain = require("./blockchain");
+const BlockChain = require('./blockchain'); // import blockchain.js
 
-const BlockChain = require('./blockchain');
-const bitcoin = new BlockChain();
+const bitcoin = new BlockChain(); // new instance of blockchain object
+
+//fake blocks
 bitcoin.createNewBlock(10257,"GHGURFUFU", 'FSDFSDF45216')
 bitcoin.createNewBlock(45120,"GFD4SFSD4", 'RGRG565GR6D6')
-console.log(bitcoin)
+
+//we are creating a new transaction, fake transactions
+bitcoin.createNewtransaction(100, "ALEX4878GJGIKGIKUGI", "JEN789645KGKGIYG");
+
+//mining a new block // new transactions are inside the teansactions array
+bitcoin.createNewBlock(123456, "000FHKEFHKUEF", "JKJK0000UG")
+
+console.log(bitcoin.chain)
