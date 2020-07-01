a3cbea62b62599593850d9910c7500668898f41a
figueroar <raul.a.figueroa@hotmail.com>
2020-06-30 20:36:53 -0400

    hashBlock() tested

--- dev/test.js
+++ dev/test.js
@@ -1,15 +1,27 @@
-const BlockChain = require('./blockchain'); // import blockchain.js
+const BlockChain = require('./blockchain'); // import blockchain.js || data structure
 
 const bitcoin = new BlockChain(); // new instance of blockchain object
 
-//fake blocks
-bitcoin.createNewBlock(10257,"GHGURFUFU", 'FSDFSDF45216')
-bitcoin.createNewBlock(45120,"GFD4SFSD4", 'RGRG565GR6D6')
+// WE will create a hash
+// variables for our method to ingest
+const previousBlockHash = 'HJHJF4156834HJ'
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'EYEHFGFKHFJF8796',
+        recipient: 'TYUIYIUGKB8964654'
+    },
+    {
+        amount: 48,
+        sender: 'RTFGFVGHVJ484',
+        recipient: 'TYIGUVJBHGKB8964654'
+    },
+    {
+        amount: 100,
+        sender: 'BUGVYHGB4516458',
+        recipient: 'GHJIUGKB8964654'
+    }
+]
+const nonce = 100;
 
-//we are creating a new transaction, fake transactions
-bitcoin.createNewtransaction(100, "ALEX4878GJGIKGIKUGI", "JEN789645KGKGIYG");
-
-//mining a new block // new transactions are inside the teansactions array
-bitcoin.createNewBlock(123456, "000FHKEFHKUEF", "JKJK0000UG")
-
-console.log(bitcoin.chain)
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce))
