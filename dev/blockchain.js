f8cfa1419b23f0fcc8d06b9fc1876ab353112b59
figueroar <raul.a.figueroa@hotmail.com>
2020-07-05 17:17:08 -0400

    genesis block created

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -7,6 +7,8 @@ const sha256 = require("sha256")
 function Blockchain() {
   this.chain = [];
   this.pendingTransactions = [];
+
+  this.createNewBlock(100, '0', '0');
 }
 ///////////////////////////////////////////////
 
--- dev/test.js
+++ dev/test.js
@@ -2,26 +2,4 @@ const BlockChain = require('./blockchain'); // import blockchain.js || data stru
 
 const bitcoin = new BlockChain(); // new instance of blockchain object
 
-// WE will create a hash
-// variables for our method to ingest
-const previousBlockHash = 'HJHJF4156834HJ'
-const currentBlockData = [
-    {
-        amount: 10,
-        sender: 'EYEHFGFKHFJF8796',
-        recipient: 'TYUIYIUGKB8964654'
-    },
-    {
-        amount: 48,
-        sender: 'RTFGFVGHVJ484',
-        recipient: 'TYIGUVJBHGKB8964654'
-    },
-    {
-        amount: 100,
-        sender: 'BUGVYHGB4516458',
-        recipient: 'GHJIUGKB8964654'
-    }
-]
-
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 50923))
+console.log(bitcoin)
