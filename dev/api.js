f6f81ceefa38949e78cb538f89694009c5e37ee1
figueroar <raul.a.figueroa@hotmail.com>
2020-07-05 20:19:44 -0400

    can post transactions

--- dev/api.js
+++ dev/api.js
@@ -1,17 +1,22 @@
-var express = require('express')
-const Blockchain = require('./blockchain')
-var app = express()
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const Blockchain = require('./blockchain');
 
-// app.get('/', function(req, res) {
-//     res.send('hello World')
-// })
+const bitcoin = new Blockchain();
+
+ // if a request comes with json, les parse, so we can acces it ina ny route
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
 
 app.get('/blockchain', function(req, res) {
-    
+    res.send(bitcoin)
 })
 
 app.post('/transaction', function(req, res){
-
+    console.log(req.body)
+    const blockIndex = bitcoin.createNewtransaction(req.body.amount, req.body.sender, req.body.recipient)
+    res.json({status: `you block index is ${blockIndex}.`})
 })
 
 app.get('/mine', function(req, res){
